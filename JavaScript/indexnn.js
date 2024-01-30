11203e2e6af07f3351e5a5208286fcc7a1d6f660
CristianPalacios20 <149723840+CristianPalacios20@users.noreply.github.com>
2024-01-30 01:59:19 +0000

    get commit -m

--- JavaScript/indexnn.js
+++ JavaScript/indexnn.js
@@ -2,9 +2,32 @@ const nombre = document.getElementById('nombre');
 const email = document.getElementById("email");
 const materia = document.getElementById("materia");
 const boton = document.getElementById("enviar");
-
+const resultado = document.querySelector(".resultado");
 const h = document.querySelector('button');
 
-boton.addEventListener('click', ()=>{
-    alert('Hola');
+boton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let error = validarCampos();
+    if(error[0]){
+        resultado.innerHTML = error[1];
+        resultado.classList.add('reed');
+    }else{
+        resultado.innerHTML = 'Solicitud enviada correctamente';
+        resultado.classList.add('greeen');
+    }
 });
+
+const validarCampos=()=>{
+    let error=[];
+    if(nombre.value.length < 5 ){
+        error[0]=true;
+        error[1]='El nombre no puede contener menos de 5 caracteres';
+        return error;
+    }else if(nombre.value.length > 40){
+        error[0]=true;
+        error[1]='El nombre no puede contener más de 40 caracteres';
+        return error;
+    }
+    error[0]=false;
+    return error;
+}
