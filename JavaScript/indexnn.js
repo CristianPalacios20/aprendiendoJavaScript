0e6d6d6362cb369c4e8f1292c9b1cb879667b911
CristianPalacios20 <149723840+CristianPalacios20@users.noreply.github.com>
2024-02-01 01:22:19 +0000

    git commit -main

--- JavaScript/indexnn.js
+++ JavaScript/indexnn.js
@@ -3,29 +3,28 @@ const email = document.getElementById("email");
 const materia = document.getElementById("materia");
 const boton = document.getElementById("enviar");
 const resultado = document.querySelector(".resultado");
-const h = document.querySelector('button');
 
 boton.addEventListener('click', (e)=>{
     e.preventDefault();
     let error = validarCampos();
     if(error[0]){
-        resultado.innerHTML = error[1];
-        resultado.classList.add('reed');
+        resultado.innerHTLM = error[1];
+        resultado.classList.add('rojo')
     }else{
-        resultado.innerHTML = 'Solicitud enviada correctamente';
-        resultado.classList.add('greeen');
+        resultado.innerHTML = 'Solicitud enviada';
+        resultado.classList.add('verde')
     }
 });
 
 const validarCampos=()=>{
-    let error=[];
-    if(nombre.value.length < 5 ){
-        error[0]=true;
-        error[1]='El nombre no puede contener menos de 5 caracteres';
+    let error = [];
+    if(nombre.value.length < 5){
+        error[0] = true;
+        error[1] = 'El nombre debe de teber más de 4 caracteres';
         return error;
     }else if(nombre.value.length > 40){
-        error[0]=true;
-        error[1]='El nombre no puede contener más de 40 caracteres';
+        error[0] = true;
+        error[1] = 'El nombre no puede tener más de 40 caracteres';
         return error;
     }
     error[0]=false;
