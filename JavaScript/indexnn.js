b256758806a6bd14c367549132efeefe8efeaa3f
CristianPalacios20 <149723840+CristianPalacios20@users.noreply.github.com>
2024-02-01 03:13:44 +0000

    git commit -main

--- JavaScript/indexnn.js
+++ JavaScript/indexnn.js
@@ -3,30 +3,41 @@ const email = document.getElementById("email");
 const materia = document.getElementById("materia");
 const boton = document.getElementById("enviar");
 const resultado = document.querySelector(".resultado");
+const rojo = document.querySelector(".rojo");
+const verde = document.querySelector(".verde");
 
 boton.addEventListener('click', (e)=>{
     e.preventDefault();
     let error = validarCampos();
     if(error[0]){
         resultado.innerHTLM = error[1];
-        resultado.classList.add('rojo');
+        // resultado.classList.add('rojo');
+        resultado.classList('.rojo')
     }else{
         resultado.innerHTML = 'Solicitud enviada';
-        resultado.classList.add('verde');
+        // resultado.classList.add('verde');
+        resultado.classList('.verde')
     }
 });
 
-const validarCampos=()=>{
+const validarCampos = ()=>{
     let error = [];
     if(nombre.value.length < 5){
         error[0] = true;
-        error[1] = 'El nombre debe de teber más de 4 caracteres';
+        error[1] = 'El nombre debe de teber más de 5 caracteres';
         return error;
     }else if(nombre.value.length > 40){
         error[0] = true;
         error[1] = 'El nombre no puede tener más de 40 caracteres';
         return error;
     }
+    // else if(email.value.length > 4 || 
+    //          email.value.length > 40 || 
+    //          email.indexOf('@Email.com')== -1){
+            
+    //     error[0] = true;
+    //     error[1] = 'El email no es válido';
+    // }
     error[0]=false;
     return error;
 }
